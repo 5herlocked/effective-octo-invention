196d088cc53236acafa6d237ec513caf9bce0a10
Shadow <cam.v737@gmail.com>
2018-09-07 07:58:45 -0400

    Updating Stuff

---
+++ MPL.java
@@ -0,0 +1,12 @@
+public class PlayListEntry {
+	private String title = "", String artist = "";
+	private int playCount = 0;
+
+	public String getTitle () { return title; }
+	public String getArtist () { return artist; }
+	public int getPlayCount () { return playCount; }
+
+	public void setTitle (String title) { this.title = title; }
+	public void setArtist (String artist) { this.artist = artist; }
+	public void setPlayCount (int playCount) { this.playCount = playCount } 
+}
