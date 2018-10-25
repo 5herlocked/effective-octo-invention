9c62e80a1b36bdf70acba92a38550e7255cc4128
Shadow <cam.v737@gmail.com>
2018-10-25 13:36:21 -0400

    Lab 10

--- Lab10/src/Lab10.java
+++ Lab10/src/Lab10.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Lab10 {
+
     public static void main (String ... args) {
         int[] arrayseqential = {0, 1, 2, 3, 4, 5, 7, 10, 19, 21, 49};
         int[] arraybinary = {-11, -7, -1, 0, 1, 4, 6, 9, 13, 21, 49, 69};
@@ -14,13 +15,13 @@ public class Lab10 {
 
         System.out.println("Input Array: " + Arrays.toString(arraybinary));
         targetIndex = binarySearch(arraybinary, -7);
-        System.out.println("(Non Recursive) Target -7 " + (targetIndex == -1 ? "was not found" : ("was found at index " + targetIndex)));
+        System.out.println("(Non Recursive) Target -7 " + (targetIndex < 0 ? "was not found and should be at index" + targetIndex : ("was found at index " + targetIndex)));
         targetIndex = binarySearch(arraybinary, 42);
-        System.out.println("(Non Recursive) Target 42 " + (targetIndex == -1 ? "was not found" : ("was found at index " + targetIndex)));
+        System.out.println("(Non Recursive) Target 42 " + (targetIndex < 0 ? "was not found and should be at index " + targetIndex : ("was found at index " + targetIndex)));
         targetIndex = recursiveBinary(arraybinary, 4);
-        System.out.println("(Recursive) Target 4 " + (targetIndex == -1 ? "was not found" : ("was found at index " + targetIndex)));
+        System.out.println("(Recursive) Target 4 " + (targetIndex == -1 ? "was not found " : ("was found at index " + targetIndex)));
         targetIndex = recursiveBinary(arraybinary, -6);
-        System.out.println("(Recursive) Target -6 " + (targetIndex == -1 ? "was not found" : ("was found at index " + targetIndex)));
+        System.out.println("(Recursive) Target -6 " + (targetIndex == -1 ? "was not found " : ("was found at index " + targetIndex)));
     }
 
     // returns the index of the target, if not found returns -1
@@ -34,16 +35,17 @@ public class Lab10 {
 
     //returns the index of the target, if not found returns -1 through binary search (non-recursive)
     private static int binarySearch (int[] array, int target) {
-        int min = 0, max = array.length-1;
+        int min = 0, max = array.length-1, mid = 0;
 
         while (min <= max) {
-            int mid = (max + min)/2;
-            if (array[mid] == target) return mid;
+            mid = (max + min)/2;
+            if (array[mid] == target) {
+                return mid;
+            }
             else if (array[mid] < target) min = mid + 1;
             else max = mid - 1;
         }
-
-        return -1;
+        return -(mid+1);
     }
 
     //return the index of the target, if not found returns -1 through binary search (recursive)
